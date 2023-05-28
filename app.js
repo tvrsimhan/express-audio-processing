fda7565ded8bfdecf837bfc6a113d1388027bbb6
tharunoptimus <tharunoptimus@outlook.com>
2023-05-28 21:49:47 +0530

    Imports express-fileupload

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 import express from "express"
 import { client } from "@gradio/client"
 
+import { createRequire } from "module"
+const require = createRequire(import.meta.url)
+let fileupload = require("express-fileupload")
+
