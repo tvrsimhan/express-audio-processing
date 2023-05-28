d744e48478a7f214b265dadb7218ca670f710487
tharunoptimus <tharunoptimus@outlook.com>
2023-05-28 21:50:04 +0530

    Added simple endpoint

--- app.js
+++ app.js
@@ -7,3 +7,19 @@ let fileupload = require("express-fileupload")
 
 import { cors } from "./middleware.js"
 
+const app = express()
+const PORT = process.env.PORT || 3003
+const SPACE_ENDPOINT = "https://tvrsimhan-yamnetapi.hf.space/"
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(fileupload())
+
+app.get("/", cors, (_, res) => {
+	res.status(200).send("API Online!")
+})
+
